b7ae78c53de223b7d7033357f92e9a30c16ad326
vallyberry <andrekellie267@gmail.com>
2022-12-13 09:39:34 -0800

    simple code to make betty happy

---
+++ val.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is a piece of code to list all numbers from 1 - 20.
+ * @i; given variable.
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 20; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
